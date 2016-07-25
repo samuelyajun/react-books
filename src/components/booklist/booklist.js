decc6ae6127cd4075f87f8dfc359db5ee82dca35
Yajun Liu <yliu@catalystsolves.com>
2016-07-25 10:17:08 -0700

    creat booklist component

--- src/components/booklist/booklist.js
+++ src/components/booklist/booklist.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import BookList from './booklist/booklist'
+
 
 class App extends Component {
     constructor(props) {
@@ -13,7 +13,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <BookList />
+                BookList   
             </div>
         );
     }
