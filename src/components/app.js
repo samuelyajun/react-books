b11df75cb18364e98431e429b7e5488b25fe72c2
Yajun Liu <yliu@catalystsolves.com>
2016-07-25 10:03:11 -0700

    create template files

---
+++ src/components/app.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        
+        };
+}
+
+    render() {
+        return (
+            <div>
+             Hello World from Yajun 1---!
+            </div>
+        );
+    }
+}
+
+export default App;
